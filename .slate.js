2fd9d72f58a1a74e54d5ae1654f511f6306945ce
Lucas Polonio <lucas.polonio@thescore.com>
2017-01-19 00:23:38 -0500

    comment out a lot of slate bindings

--- .slate.js
+++ .slate.js
@@ -90,10 +90,7 @@ var twoMonitorLayout = S.lay("twoMonitor", {
   "Google Chrome" : tboltChromeHash,
   "iTerm2" : tboltFullHash,
   "Slack" : lapFullHash,
-  "Spotify" : {
-    "operations" : [tboltFull],
-    "repeat" : true
-  }
+  "Spotify" : tboltChromeHash
 });
 
 // 1 monitor layout
@@ -142,51 +139,51 @@ S.bnda({
   "space:ctrl" : universalLayout,
 
   // Basic Location Bindings
-  "pad0:ctrl" : lapChat,
-  "[:ctrl" : lapChat,
-  "pad.:ctrl" : lapMain,
-  "]:ctrl" : lapMain,
-  "pad1:ctrl" : tboltLeftBot,
-  "pad2:ctrl" : tboltMidBot,
-  "pad3:ctrl" : tboltRightBot,
-  "pad4:ctrl" : tboltLeftTop,
-  "pad5:ctrl" : tboltMidTop,
-  "pad6:ctrl" : tboltRightTop,
-  "pad7:ctrl" : tboltLeft,
-  "pad8:ctrl" : tboltMid,
-  "pad9:ctrl" : tboltRight,
-  "pad=:ctrl" : tboltFull,
+  // "pad0:ctrl" : lapChat,
+  // "[:ctrl" : lapChat,
+  // "pad.:ctrl" : lapMain,
+  // "]:ctrl" : lapMain,
+  // "pad1:ctrl" : tboltLeftBot,
+  // "pad2:ctrl" : tboltMidBot,
+  // "pad3:ctrl" : tboltRightBot,
+  // "pad4:ctrl" : tboltLeftTop,
+  // "pad5:ctrl" : tboltMidTop,
+  // "pad6:ctrl" : tboltRightTop,
+  // "pad7:ctrl" : tboltLeft,
+  // "pad8:ctrl" : tboltMid,
+  // "pad9:ctrl" : tboltRight,
+  // "pad=:ctrl" : tboltFull,
 
   // Resize Bindings
   // NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
-  "right:ctrl" : S.op("resize", { "width" : "+10%", "height" : "+0" }),
-  "left:ctrl" : S.op("resize", { "width" : "-10%", "height" : "+0" }),
-  "up:ctrl" : S.op("resize", { "width" : "+0", "height" : "-10%" }),
-  "down:ctrl" : S.op("resize", { "width" : "+0", "height" : "+10%" }),
-  "right:alt" : S.op("resize", { "width" : "-10%", "height" : "+0", "anchor" : "bottom-right" }),
-  "left:alt" : S.op("resize", { "width" : "+10%", "height" : "+0", "anchor" : "bottom-right" }),
-  "up:alt" : S.op("resize", { "width" : "+0", "height" : "+10%", "anchor" : "bottom-right" }),
-  "down:alt" : S.op("resize", { "width" : "+0", "height" : "-10%", "anchor" : "bottom-right" }),
+  //"right:ctrl" : S.op("resize", { "width" : "+10%", "height" : "+0" }),
+  //"left:ctrl" : S.op("resize", { "width" : "-10%", "height" : "+0" }),
+  //"up:ctrl" : S.op("resize", { "width" : "+0", "height" : "-10%" }),
+  //"down:ctrl" : S.op("resize", { "width" : "+0", "height" : "+10%" }),
+  //"right:alt" : S.op("resize", { "width" : "-10%", "height" : "+0", "anchor" : "bottom-right" }),
+  //"left:alt" : S.op("resize", { "width" : "+10%", "height" : "+0", "anchor" : "bottom-right" }),
+  //"up:alt" : S.op("resize", { "width" : "+0", "height" : "+10%", "anchor" : "bottom-right" }),
+  //"down:alt" : S.op("resize", { "width" : "+0", "height" : "-10%", "anchor" : "bottom-right" }),
 
   // Push Bindings
   // NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
-  "right:ctrl;shift" : S.op("push", { "direction" : "right", "style" : "bar-resize:screenSizeX/2" }),
-  "left:ctrl;shift" : S.op("push", { "direction" : "left", "style" : "bar-resize:screenSizeX/2" }),
-  "up:ctrl;shift" : S.op("push", { "direction" : "up", "style" : "bar-resize:screenSizeY/2" }),
-  "down:ctrl;shift" : S.op("push", { "direction" : "down", "style" : "bar-resize:screenSizeY/2" }),
+  // "right:ctrl;shift" : S.op("push", { "direction" : "right", "style" : "bar-resize:screenSizeX/2" }),
+  // "left:ctrl;shift" : S.op("push", { "direction" : "left", "style" : "bar-resize:screenSizeX/2" }),
+  // "up:ctrl;shift" : S.op("push", { "direction" : "up", "style" : "bar-resize:screenSizeY/2" }),
+  // "down:ctrl;shift" : S.op("push", { "direction" : "down", "style" : "bar-resize:screenSizeY/2" }),
 
   // Nudge Bindings
   // NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
-  "right:ctrl;alt" : S.op("nudge", { "x" : "+10%", "y" : "+0" }),
-  "left:ctrl;alt" : S.op("nudge", { "x" : "-10%", "y" : "+0" }),
-  "up:ctrl;alt" : S.op("nudge", { "x" : "+0", "y" : "-10%" }),
-  "down:ctrl;alt" : S.op("nudge", { "x" : "+0", "y" : "+10%" }),
+  // "right:ctrl;alt" : S.op("nudge", { "x" : "+10%", "y" : "+0" }),
+  // "left:ctrl;alt" : S.op("nudge", { "x" : "-10%", "y" : "+0" }),
+  // "up:ctrl;alt" : S.op("nudge", { "x" : "+0", "y" : "-10%" }),
+  // "down:ctrl;alt" : S.op("nudge", { "x" : "+0", "y" : "+10%" }),
 
   // Throw Bindings
   // NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
-  "pad1:ctrl;alt" : S.op("throw", { "screen" : "2", "width" : "screenSizeX", "height" : "screenSizeY" }),
-  "pad2:ctrl;alt" : S.op("throw", { "screen" : "1", "width" : "screenSizeX", "height" : "screenSizeY" }),
-  "pad3:ctrl;alt" : S.op("throw", { "screen" : "0", "width" : "screenSizeX", "height" : "screenSizeY" }),
+  // "pad1:ctrl;alt" : S.op("throw", { "screen" : "2", "width" : "screenSizeX", "height" : "screenSizeY" }),
+  // "pad2:ctrl;alt" : S.op("throw", { "screen" : "1", "width" : "screenSizeX", "height" : "screenSizeY" }),
+  // "pad3:ctrl;alt" : S.op("throw", { "screen" : "0", "width" : "screenSizeX", "height" : "screenSizeY" }),
   "right:ctrl;alt;cmd" : S.op("throw", { "screen" : "right", "width" : "screenSizeX", "height" : "screenSizeY" }),
   "left:ctrl;alt;cmd" : S.op("throw", { "screen" : "left", "width" : "screenSizeX", "height" : "screenSizeY" }),
   "up:ctrl;alt;cmd" : S.op("throw", { "screen" : "up", "width" : "screenSizeX", "height" : "screenSizeY" }),
@@ -194,15 +191,15 @@ S.bnda({
 
   // Focus Bindings
   // NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
-  "k:cmd;alt" : S.op("focus", { "direction" : "behind" }),
-  "j:cmd;alt" : S.op("focus", { "direction" : "behind" }),
-  "up:cmd" : S.op("focus", { "direction" : "up" }),
-  "down:cmd" : S.op("focus", { "direction" : "down" }),
-  "up:cmd;alt" : S.op("focus", { "direction" : "behind" }),
-  "down:cmd;alt" : S.op("focus", { "direction" : "behind" }),
+  // "k:cmd;alt" : S.op("focus", { "direction" : "behind" }),
+  // "j:cmd;alt" : S.op("focus", { "direction" : "behind" }),
+  // "up:cmd" : S.op("focus", { "direction" : "up" }),
+  // "down:cmd" : S.op("focus", { "direction" : "down" }),
+  // "up:cmd;alt" : S.op("focus", { "direction" : "behind" }),
+  // "down:cmd;alt" : S.op("focus", { "direction" : "behind" }),
 
   // Window Hints
-  "esc:cmd" : S.op("hint"),
+  // "esc:cmd" : S.op("hint"),
 
   // Switch currently doesn't work well so I'm commenting it out until I fix it.
   //"tab:cmd" : S.op("switch"),
